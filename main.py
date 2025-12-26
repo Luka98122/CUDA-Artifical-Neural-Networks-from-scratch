8cbc28cc713c030bfe83f438e40c014b16b52693
luka98122 <luka98122@gmail.com>
2025-12-26 23:06:32 +0100

    Small re-structure

--- main.py
+++ main.py
@@ -16,17 +16,22 @@ inputs = [1, 2, 3, 2.5]
 weights1 = [0.2, 0.8, -0.5, 1]
 weights2 = [0.5, -0.91, 0.26, -0.5]
 weights3 = [-0.26, -0.27, 0.17, 0.87]
+weights = [weights1,weights2,weights3]
+
 bias1 = 2
 bias2 = 3
 bias3 = 0.5    
- 
+biases = [bias1,bias2,bias3]
+
 """ 
 All neurons in a layer have the same inputs (the output of the previous layer),
 but output different values because of their unique weights and biases.
 """
 
-neuron1 = Neuron(inputs,weights1,bias1)
-neuron2 = Neuron(inputs,weights2,bias2)
-neuron3 = Neuron(inputs,weights3,bias3)
+layer_outputs = []
+neurons = []
+for i in range(3):
+    neurons.append(Neuron(inputs,weights[i],biases[i]))
+    layer_outputs.append(neurons[-1].output)
 
-print([neuron1.output,neuron2.output,neuron3.output])
+print(layer_outputs)
