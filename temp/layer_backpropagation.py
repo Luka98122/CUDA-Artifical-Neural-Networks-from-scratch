46044b665cfcfc203d283b2eb63f3afd2fb9aae2
luka98122 <luka98122@gmail.com>
2026-01-04 15:26:46 +0100

    Need to merge backprop with OOP

--- temp/layer_backpropagation.py
+++ temp/layer_backpropagation.py
@@ -1,1 +1,32 @@
+import numpy as np
 # TODO: Implement layer back propagation (pg. ~201)
+
+# Passed in gradient from the next layer
+# for the purpose of this example we're going to use
+# a vector of 1s
+dvalues = np.array([[1., 1., 1.],
+                    [2., 2., 2.],
+                    [3., 3., 3.]])
+
+weights = np.array([[0.2, 0.8, -0.5, 1],
+                    [0.5, -0.91, 0.26, -0.5],
+                    [-0.26, -0.27, 0.17, 0.87]])
+weights = weights.T
+
+dx0 = sum(weights[0]*dvalues[0])
+dx1 = sum(weights[1]*dvalues[0])
+dx2 = sum(weights[2]*dvalues[0])
+dx3 = sum(weights[3]*dvalues[0])
+
+#dinputs = np.array([dx0, dx1, dx2, dx3])
+dinputs = np.dot(dvalues,weights.T)
+print(dinputs)
+
+
+inputs = np.array([[1, 2, 3, 2.5],
+                   [2., 5., -1., 2],
+                   [-1.5, 2.7, 3.3, -0.8]])
+# sum weights of given input
+# and multiply by the passed in gradient for this neuron
+dweights = np.dot(inputs.T, dvalues)
+print(dweights)
