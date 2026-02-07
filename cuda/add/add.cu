5a0ed30663a6d5d8e9de02eeec69838ca5e6448c
luka98122 <luka98122@gmail.com>
2026-02-07 19:04:15 +0100

    Cleaned up demo cuda functions

--- cuda/add/add.cu
+++ cuda/add/add.cu
@@ -3,16 +3,6 @@
 #include <stdint.h>
 
 
-__global__ void add_kernel(float *a, float *b, float *c)
-{
-    *c = *a + *b;
-}
-
-__global__ void multiply_kernel(float *a, float *b, float *c)
-{
-    *c = (*a) * (*b);
-}
-
 __global__ void matAdd(const float* A,
                        const float* B,
                        float* C,
@@ -29,56 +19,6 @@ __global__ void matAdd(const float* A,
 }
 
 
-extern "C" __declspec(dllexport)
-void add_cuda(float *a, float *b, float *result)
-{
-    cudaSetDevice(0);
-    float* d_a = nullptr;
-    float* d_b = nullptr;
-    cudaMalloc(&d_a, sizeof(float));
-    cudaMalloc(&d_b, sizeof(float));
-
-    cudaMemcpy(d_a,a,sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b,b,sizeof(float), cudaMemcpyHostToDevice);
-    
-    float *d_c = nullptr;
-    cudaMalloc(&d_c, sizeof(float));
-
-    add_kernel<<<1, 1>>>(d_a, d_b, d_c);
-    cudaGetLastError();
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(result, d_c, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaFree(d_c);
-    cudaFree(d_a);
-    cudaFree(d_b);
-}
-
-extern "C" __declspec(dllexport)
-void multiply_cuda(float *a, float *b, float *result)
-{
-    cudaSetDevice(0);
-    float* d_a = nullptr;
-    float* d_b = nullptr;
-    cudaMalloc(&d_a, sizeof(float));
-    cudaMalloc(&d_b, sizeof(float));
-
-    cudaMemcpy(d_a,a,sizeof(float), cudaMemcpyHostToDevice);
-    cudaMemcpy(d_b,b,sizeof(float), cudaMemcpyHostToDevice);
-    
-    float *d_c = nullptr;
-    cudaMalloc(&d_c, sizeof(float));
-
-    multiply_kernel<<<1, 1>>>(d_a, d_b, d_c);
-    cudaGetLastError();
-    cudaDeviceSynchronize();
-
-    cudaMemcpy(result, d_c, sizeof(float), cudaMemcpyDeviceToHost);
-    cudaFree(d_c);
-    cudaFree(d_a);
-    cudaFree(d_b);
-
-}
 
 extern "C" __declspec(dllexport)
 void mat_add_cuda(const float* h_A,
