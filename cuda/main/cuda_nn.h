9a0a85738fae205fb00f8b0215fc7a93321cfd2a
Luka Markovic <luka98122@gmail.com>
2026-02-21 22:46:33 +0100

    Slowly switching CUDA to OOP

--- cuda/main/cuda_nn.h
+++ cuda/main/cuda_nn.h
@@ -18,4 +18,23 @@ CUDA_API void argmax_cuda(const float* h_a, int* result_idx, int n);
 
 CUDA_API void relu_cuda(float* h_input, int n);
 CUDA_API void relu_cuda_100(float* h_input, int n);
+
+
+// Switch to OOP:
+CUDA_API void* create_dense_layer(int n_inputs, int n_outputs, const float* h_weights, const float* h_bias) {
+    return new DenseLayer(n_inputs, n_outputs, h_weights, h_bias);
+}
+
+CUDA_API void destroy_dense_layer(void* layer_ptr) {
+    if (layer_ptr != nullptr) {
+        delete static_cast<DenseLayer*>(layer_ptr);
+    }
+}
+
+CUDA_API float* forward_dense_layer(void* layer_ptr, const float* d_input) {
+    DenseLayer* layer = static_cast<DenseLayer*>(layer_ptr);
+    return layer->forward(d_input);
+}
+
+
 #endif // CUDA_NN_H
--- cuda/main/nn_layers.cu
+++ cuda/main/nn_layers.cu
@@ -5,6 +5,7 @@
 // ---------------------------
 // Forward Layer (Dense)
 // ---------------------------
+
 __global__ void forward_layer_kernel(const float* input, const float* weights, const float* bias, float* output, int n_inputs, int n_outputs)
 {
     // TODO: Implement shared memory cache
@@ -20,6 +21,56 @@ __global__ void forward_layer_kernel(const float* input, const float* weights, c
         output[row] = sum + bias[row];
     }
 }
+
+// OOP layer
+
+class DenseLayer {
+private:
+    int n_in;
+    int n_out;
+    
+    float* d_weights;
+    float* d_bias;
+    float* d_output;
+
+public:
+    //constructor (initialise)
+    DenseLayer(int n_inputs, int n_outputs, const float* h_weights, const float* h_bias) 
+        : n_in(n_inputs), n_out(n_outputs) {
+        
+        size_t size_out = n_out * sizeof(float);
+        size_t size_weights = n_in * n_out * sizeof(float);
+
+        cudaMalloc(&d_weights, size_weights);
+        cudaMalloc(&d_bias, size_out);
+        cudaMalloc(&d_output, size_out);
+
+        cudaMemcpy(d_weights, h_weights, size_weights, cudaMemcpyHostToDevice);
+        cudaMemcpy(d_bias, h_bias, size_out, cudaMemcpyHostToDevice);
+    }
+
+    // destructor
+    ~DenseLayer() {
+        cudaFree(d_weights);
+        cudaFree(d_bias);
+        cudaFree(d_output);
+    }
+
+    // takes a device pointer for input, returns a device pointer for output
+    float* forward(const float* d_input) {
+        int threadsPerBlock = 256;
+        int blocksPerGrid = (n_out + threadsPerBlock - 1) / threadsPerBlock;
+
+        forward_layer_kernel<<<blocksPerGrid, threadsPerBlock>>>(
+            d_input, d_weights, d_bias, d_output, n_in, n_out
+        );
+        cudaDeviceSynchronize();
+        
+        return d_output;
+    }
+};
+
+// Old forward layer
 
 void forward_layer_cuda(const float* h_input, const float* h_weights, const float* h_bias, float* h_output, int n_inputs, int n_outputs)
 {
